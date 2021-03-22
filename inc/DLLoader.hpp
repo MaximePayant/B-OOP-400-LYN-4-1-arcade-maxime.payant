81ae008bc01d759e95d1a3e3c7a4b9b1f7a01e49
Guillaume Soisson <guillaume.soisson@epitech.eu>
2021-03-22 16:59:34 +0100

    Initial commit

---
+++ inc/DLLoader.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2021
+** DLLoader.hpp.h
+** File description:
+** DLLoader.hpp.h
+*/
+
+#ifndef B_OOP_400_LYN_4_1_ARCADE_GUILLAUME_SOISSON_DLLOADER
+#define B_OOP_400_LYN_4_1_ARCADE_GUILLAUME_SOISSON_DLLOADER
+
+
+class DLLoader
+{
+
+};
+
+
+#endif //B_OOP_400_LYN_4_1_ARCADE_GUILLAUME_SOISSON_DLLOADER
---
+++ src/DLLoader.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2021
+** DLLoader.cpp.cc
+** File description:
+** DLLoader.cpp.cc
+*/
+
+#include "DLLoader.hpp"
